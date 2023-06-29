9c758581a290eefabd1bd31539f44b0ae5b6c916
Denis <denis30510@gmail.com>
2023-06-29 17:48:10 +0300

    added utils in main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,18 @@
+from utils import update_info, send_alarm
+from telegram import telegram
+from threading import Thread
 
 
 if __name__ == '__main__':
-    pass
+    tel = Thread(target=telegram)
+    info = Thread(target=update_info)
+    alarm = Thread(target=send_alarm)
+
+    tel.start()
+    info.start()
+    alarm.start()
+    tel.join()
+    info.join()
+    alarm.join()
 
 
--- src/utils.py
+++ src/utils.py
@@ -1,20 +1,62 @@
-from threading import Timer
+import time
+import os
+import ujson
+from whois import whois
 from src.saver import Saver
 from datetime import datetime
-
+from whois_search import WhoisSearch
+from threading import Timer
+import telebot
 
 saver = Saver()
 format ='%Y-%m-%d %H:%M:%S'
+path_file = os.path.join('..', 'src', 'chat_id.json')
+api = os.environ.get('API_TELEBOT')
+bot = telebot.TeleBot(api)
 
 
-def update_info():
-    pass
-
-
-def send_alarm():
+def update_info() -> None:
+    """
+    Обновляет информацию о доменах раз в день
+    :return: None
+    """
     date = saver.get_info_file()[0]
     if len(date) > 0:
-        return [f'У домена {i} осталось {(datetime.strptime(k, format) - datetime.now()).days} дней!' \
-                for i, k in date.items() if (datetime.strptime(k, format) - datetime.now()).days < 14]
+        # перебор из файла доменов
+        for i in date.keys():
+            time.sleep(0.5)
+            try:
+                value = whois(i).expiration_date
+                if value is not None:
+                    if type(value) is list:
+                        value = value[1]
+                    domain = {i.upper(): str(value)}
+                    saver.adding_info_file(domain)
+                else:
+                    whois_search = WhoisSearch(i)
+                    domain = {i.upper(): whois_search.get_date()}
+                    saver.adding_info_file(domain)
+            except Exception:
+                pass
+    Timer(86400, update_info).start()
+
+
+def send_alarm() -> None:
+    """
+    Раз в день посылает в чат если аренда домена меньше 14 дней
+    :return: None
+    """
+    date = saver.get_info_file()[0]
+    if len(date) > 0:
+        # список доменов с истекающим сроком
+        join = [f'У домена {i} осталось {(datetime.strptime(k, format) - datetime.now()).days} дней!' \
+                for i, k in date.items() if (datetime.strptime(k, format) - datetime.now()).days <= 14]
+        if len(join) > 0:
+            file = open(path_file, 'r', encoding='utf-8')
+            test_json = ujson.load(file)
+            file.close()
+            for i in test_json:
+                bot.send_message(i, '\n'.join(join))
+    Timer(86400, send_alarm).start()
 
 
